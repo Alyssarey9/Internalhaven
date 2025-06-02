a64adb2047db6d9b1fee70cf3c456148f877f25a
Jerry Pierre <jerry@traderthings.com>
2025-06-01 22:47:47 -0400

    Enhanced the webfont to Bellota - added animation classes for the font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Bellota:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" 
+          rel="stylesheet" 
+        />
+      </head>
+      <body className="antialiased font-bellota">
         {children}
       </body>
     </html>
